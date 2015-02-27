0270f456ef56acc8712ff1865722a5b18a304b58
Konstantinos Theofilatos <konstantinos.theofilatos@cern.ch>
2015-02-27 11:11:43 +0100

    version before cleanning up beginJob, endJob and fill descriptions

--- plugins/SimpleROOT.cc
+++ plugins/SimpleROOT.cc
@@ -29,6 +29,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
@@ -42,66 +44,54 @@
 using namespace std;
 
 class SimpleROOT : public edm::EDAnalyzer {
-   public:
-      explicit SimpleROOT(const edm::ParameterSet&);
-      ~SimpleROOT();
+    public:
+    	explicit SimpleROOT(const edm::ParameterSet&);
+      	~SimpleROOT();
+        
 
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+//	static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 
-   private:
-      virtual void beginJob() override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-      virtual void endJob() override;
+    private:
+//	virtual void beginJob() override;
+	virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+        virtual void reset();
+//	virtual void endJob() override;
 
-      //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-      //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-
-      TTree *events;
-
-      // ----------member data ---------------------------
-      UShort_t numVtx; // 16-bit integer 
+	edm::Service<TFileService> fileService_; 
+	TTree *events_;
+	// ----------member data 
+	// to add more variable you should always edit 3 checkpoints of this file 
+	// (give typedef, include it in the branch, initialization for each event)
+	
+		
+        // --- CheckPoint 1 --- 
+	UChar_t numVtx_; // 8-bit integer denoted as "b" in the leaflist of the branch
+      
 };
 
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
-
-//
-// constructors and destructor
-//
 SimpleROOT::SimpleROOT(const edm::ParameterSet& iConfig)
-
 {
-   //now do what ever initialization is needed
-
+    // --- CheckPoint 2 --- 
+    events_ = fileService_->make<TTree>("events","events");
+    events_->Branch("numVtx_",&numVtx_,"numVtx_/b");
 }
 
-
-SimpleROOT::~SimpleROOT()
+void SimpleROOT::reset()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-
+    // --- CheckPoint 3 --- 
+    numVtx_ = 0;
 }
 
+SimpleROOT::~SimpleROOT() {}
 
-//
-// member functions
-//
+
 
 // ------------ method called for each event  ------------
-void
-SimpleROOT::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+void SimpleROOT::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-    using namespace edm;
+    reset(); // --- initialize all variables to be stored!
+    //using namespace edm;
 
     // --- get MiniAOD collections -- names of the collection are hard-coded (no need to read them from python as they don't change so frequently)
     edm::Handle<reco::VertexCollection> vertices;
@@ -122,71 +112,36 @@ SimpleROOT::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     // --- loop inside the objects
     const reco::Vertex &PV = vertices->front();  // get event's primary vertex
     bool PVisValid = PV.isValid();
-    numVtx = UShort_t(vertices->size());
+    numVtx_ = UChar_t(vertices->size());
     
     cout << "PVisValid = " << PVisValid << endl;
 
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
+    events_->Fill();
 }
 
 
-// ------------ method called once each job just before starting event loop  ------------
-void 
-SimpleROOT::beginJob()
-{
-    events = new TTree("events","events");
-}
-
-// ------------ method called once each job just after ending the event loop  ------------
-void 
-SimpleROOT::endJob() 
-{
-}
-
-// ------------ method called when starting to processes a run  ------------
-/*
-void 
-SimpleROOT::beginRun(edm::Run const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when ending the processing of a run  ------------
-/*
-void 
-SimpleROOT::endRun(edm::Run const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when starting to processes a luminosity block  ------------
-/*
-void 
-SimpleROOT::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when ending the processing of a luminosity block  ------------
-/*
-void 
-SimpleROOT::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
-void
-SimpleROOT::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
-}
+//// ------------ method called once each job just before starting event loop  ------------
+//void 
+//SimpleROOT::beginJob()
+//{
+//}
+//
+//// ------------ method called once each job just after ending the event loop  ------------
+//void 
+//SimpleROOT::endJob() 
+//{
+//}
+//
+//
+//// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+//void
+//SimpleROOT::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+//  //The following says we do not know what parameters are allowed so do no validation
+//  // Please change this to state exactly what you do use, even if it is no parameters
+//  edm::ParameterSetDescription desc;
+//  desc.setUnknown();
+//  descriptions.addDefault(desc);
+//}
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(SimpleROOT);
