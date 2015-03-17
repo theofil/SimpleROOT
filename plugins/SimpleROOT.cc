a3f0f9e6982b3f37ee5122fbc360ddb47506a221
theofil <konstantinos.theofilatos@gmail.com>
2015-03-17 18:08:35 +0100

    implemented loops for all objects

--- plugins/SimpleROOT.cc
+++ plugins/SimpleROOT.cc
@@ -38,8 +38,10 @@
 #include "DataFormats/PatCandidates/interface/Electron.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "DataFormats/PatCandidates/interface/MET.h"
+#include "DataFormats/PatCandidates/interface/Photon.h"
 
 #include "TTree.h"
+#include "TLorentzVector.h"
 
 using namespace std;
 
@@ -54,6 +56,13 @@ class SimpleROOT : public edm::EDAnalyzer {
         virtual bool isGoodMuon(const pat::Muon &mu);
         virtual bool isGoodElectron(const pat::Electron &el);
         virtual bool isGoodVertex(const reco::Vertex &PV);
+        virtual bool isGoodJet(const pat::Jet &myJet);
+        virtual bool isGoodPhoton(const pat::Photon &myPhoton);
+
+
+
+        TLorentzVector P4(const reco::Candidate* cand){TLorentzVector p4vec; p4vec.SetPxPyPzE( cand->px(), cand->py(), cand->pz(), cand->energy() ); return p4vec;}
+
 
 	edm::Service<TFileService> fileService_; 
 	TTree *events_;
@@ -111,39 +120,103 @@ void SimpleROOT::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
     edm::Handle<pat::METCollection> mets;
     iEvent.getByLabel("slimmedMETs", mets);
 
+    edm::Handle<pat::PhotonCollection> photons;
+    iEvent.getByLabel("slimmedPhotons", photons);
+
+    vector<const reco::Candidate *> myLeptons; // in this container we will store all selected RECO electrons and RECO muons 
+    vector<const reco::Candidate *> myJets; // in this container we will store all prompt jets (PV)
+    vector<const reco::Candidate *> myRjets; // in this container we will all rejected prompt jets (PV) -- due to DR matching with myLeptons
+    vector<const reco::Candidate *> myPhotons; // in this container we will store all photons
+
     // --- loop inside the objects
     reco::Vertex vtx;
     for (const reco::Vertex &PV : *vertices)
     {
 	if( isGoodVertex(PV) ){ vtx = PV; break;} // get first good vertex 
     }
-    numVtx_ = UChar_t(vertices->size());
-
-    std::vector<const reco::Candidate *> leptons; // in this container we will store all selected RECO electrons and RECO muons 
 
     for (const pat::Muon &mu : *muons) 
     {
-    	// --- https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideMuonId
-	if( isGoodMuon(mu) )leptons.push_back(&mu);
+	if( isGoodMuon(mu) )myLeptons.push_back(&mu);
     }
     for (const pat::Electron &el : *electrons)
     {
-         if( isGoodElectron(el) ) leptons.push_back(&el);
+         if( isGoodElectron(el) ) myLeptons.push_back(&el);
     }
 
+    for(const pat::Jet &myjet : *jets)
+    {
+	bool isLeptonMatched = false;
+        float DRmax = 0.4;
+        for(auto & lep: myLeptons) if( P4(lep).DeltaR( P4(&myjet) ) < DRmax ) isLeptonMatched = true;
 
-    cout << "lepton size = " << leptons.size() << endl;
+	if( isGoodJet(myjet) && !isLeptonMatched ) myJets.push_back(&myjet);
+	if( isGoodJet(myjet) && isLeptonMatched ) myRjets.push_back(&myjet);
+    }
+
+    for (const pat::Photon &photon : *photons) 
+    {
+	if( isGoodPhoton(photon) ) myPhotons.push_back(&photon); 
+    }
+
+    const pat::MET &met = mets->front();
+    printf("MET: pt %5.1f, ptraw %5.1f, phi %+4.2f, sumEt (%.1f). genMET %.1f. MET with JES up/down: %.1f/%.1f\n",
+        met.pt(), met.shiftedPt(pat::MET::NoShift, pat::MET::Raw), met.phi(), met.sumEt(),
+        met.genMET()->pt(),
+        met.shiftedPt(pat::MET::JetEnUp), met.shiftedPt(pat::MET::JetEnDown));
+    // raw met use shiftedPt(pat::MET::NoShift, pat::MET::Raw) because of broken uncorrectedPt() method for Phys14
+
+    printf("\n");
+
+
+
+    //  --- sort by pt all objects, the [] is a C++11 lambda func 
+    std::sort(myLeptons.begin(), myLeptons.end(), [](const reco::Candidate * a, const reco::Candidate * b){return a->pt() > b->pt();} ); 
+    std::sort(myJets.begin(), myJets.end(), [](const reco::Candidate * a, const reco::Candidate * b){return a->pt() > b->pt();} ); 
+    std::sort(myRjets.begin(), myRjets.end(), [](const reco::Candidate * a, const reco::Candidate * b){return a->pt() > b->pt();} ); 
+
+    cout << "print all sorted objects " << endl;
+    for (auto & lep : myLeptons) cout <<"let pt = "<<  lep->pt() << " (eta, phi) = (" << lep->eta() << " , " << lep->phi() << ")" << " lep pdgId = " << lep->pdgId() << endl;
+    for(auto &myjet : myJets) cout << "jet pt = " << myjet->pt() << " (eta, phi) = (" << myjet->eta() << " , " << myjet->phi() << ")" << endl;
+    for(auto &myjet : myRjets) cout << "rjet pt = " << myjet->pt() << " (eta, phi) = (" << myjet->eta() << " , " << myjet->phi() << ")" << endl;
+    for(auto &myphoton : myPhotons) cout << "rjet pt = " << myphoton->pt() << " (eta, phi) = (" << myphoton->eta() << " , " << myphoton->phi() << ")" << endl;
+ 
+    // --- Fill branches
+    numVtx_ = UChar_t(vertices->size());
+    if( vtx.isValid() ) flagBit_ |= 1 << hasGoodPV;
 
     events_->Fill();
 }
 
-bool SimpleROOT::isGoodMuon(const pat::Muon &mu)
+bool SimpleROOT::isGoodPhoton(const pat::Photon &photon)
 {
     bool res = true; // by default is good, unless fails a cut bellow
 
-    if(mu.pt() < 10) 
-    if(fabs(mu.eta()) > 2.4) 
-    res = false;
+    cout << __LINE__ << endl;
+    if(photon.pt() < 20) res = false; 
+    if(fabs(photon.eta()) > 2.4) res = false;
+
+    return res;
+}
+
+
+bool SimpleROOT::isGoodJet(const pat::Jet &myJet)
+{
+    bool res = true; // by default is good, unless fails a cut bellow
+
+    if(myJet.pt() < 30) res = false; 
+    if(fabs(myJet.eta()) > 2.4) res = false;
+
+    return res;
+}
+
+bool SimpleROOT::isGoodMuon(const pat::Muon &mu)
+{
+    // --- https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideMuonId
+    bool res = true; // by default is good, unless fails a cut bellow
+
+    if(mu.pt() < 10) res = false; 
+    if(fabs(mu.eta()) > 2.4) res = false;
 
     return res;
 }
@@ -152,9 +225,8 @@ bool SimpleROOT::isGoodElectron(const pat::Electron &el)
 {
     bool res = true; // by default is good, unless fails a cut bellow
 
-    if(el.pt() < 10) 
-    if(fabs(el.eta()) > 2.4) 
-    res = false;
+    if(el.pt() < 10) res = false;
+    if(fabs(el.eta()) > 2.4) res = false;
 
     return res;
 }
