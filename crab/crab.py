112ce2c7fcccaf3164091241dae92738a8134f33
theofil <konstantinos.theofilatos@gmail.com>
2015-04-23 16:39:37 +0200

    commiting crab

--- crab/crab.py
+++ crab/crab.py
@@ -2,7 +2,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'ttbar_synch_v2'
+config.General.requestName = 'crab_ttbar_synch_v5_noISO'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
