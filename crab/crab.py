8f96a1059089302b73ec229e86ddf6af917cdf4a
theofil <konstantinos.theofilatos@gmail.com>
2015-04-24 14:14:57 +0200

    crab

--- crab/crab.py
+++ crab/crab.py
@@ -2,7 +2,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'ttbar_synch_v4_withISO'
+config.General.requestName = 'ttbar_miniISO_v1'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
