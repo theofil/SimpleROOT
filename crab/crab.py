047190cd4847d92c6351ba6ef94219d3d8e4ef74
theofil <konstantinos.theofilatos@gmail.com>
2015-07-23 16:21:53 +0200

    udpating crab files

--- crab/crab.py
+++ crab/crab.py
@@ -2,17 +2,18 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'MuonEG_v1c'
+config.General.requestName = 'DYm1050_v1c'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '../runme_cfg.py'
 config.Data.inputDBS = 'global'
 
-config.Data.inputDataset = '/MuonEG/Run2015B-PromptReco-v1/MINIAOD'
+
+config.Data.inputDataset = '/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/DCSOnly/json_DCSONLY_Run2015B.txt'
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 250000
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-251642_13TeV_PromptReco_Collisions15_JSON.txt'
 
 config.Data.outLFNDirBase = '/store/user/theofil/test' # or '/store/group/<subdir>'
 config.Data.publication = False
