a4e87af46fb473c4a7f9f41fea2c858e04a73f4d
theofil <konstantinos.theofilatos@gmail.com>
2015-07-24 17:40:33 +0200

    crab cfg updates

--- crab/crab.py
+++ crab/crab.py
@@ -2,7 +2,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'DYm1050_v1c'
+config.General.requestName = 'ZZTo4L_13TeV_powheg_pythia8___RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1___v1d'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
@@ -10,8 +10,8 @@ config.JobType.psetName = '../runme_cfg.py'
 config.Data.inputDBS = 'global'
 
 
-config.Data.inputDataset = '/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
-#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/DCSOnly/json_DCSONLY_Run2015B.txt'
+config.Data.inputDataset =  '/ZZTo4L_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON.txt'
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 250000
 
