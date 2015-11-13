ae46c1513bcd8b730cfc14c8afe4153172519cf4
theofil <konstantinos.theofilatos@gmail.com>
2015-11-13 13:51:51 +0100

    coolv1 (crab)

--- crab/crab.py
+++ crab/crab.py
@@ -2,7 +2,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'
+config.General.requestName = 'DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8___coolv1'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
